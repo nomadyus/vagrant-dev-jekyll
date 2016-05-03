df158f8f280e54fe51ffb3b9378d8e3d124473b2
yusuf-kami <yusuf.kami@gmail.com>
2016-05-03 01:17:18 -0400

    installing jekyll dependencies using vagrant user

--- puppet/modules/github-pages/manifests/init.pp
+++ puppet/modules/github-pages/manifests/init.pp
@@ -8,21 +8,22 @@ class github-pages {
 	}
     
     exec {'gpg-signature':
-        command => 'sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3',
+        command => "${as_vagrant} 'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3'",
         notify => Exec['install-rvm'],
     }
     
     exec {'install-rvm': 
-        command => 'sudo curl -L https://get.rvm.io | bash -s stable --ruby',
+        command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable --ruby'",
         require => [Package['curl'], Exec['gpg-signature']],
         notify => Exec['update-ruby'],
     }
     
     exec {'update-ruby':
-        command => 'rvm install 2.0.0 --autolibs=enabled && rvm --fuzzy alias create default 2.0.0',
+        command => "${as_vagrant} 'rvm install 2.0.0 --autolibs=enabled && rvm --fuzzy alias create default 2.0.0'",
         #onlyif => 'type rvm | head -1 | grep -c \'rvm is a function\' | wc -l',
         require => Exec['install-rvm'],
         notify => Exec['install-bundler'],
+        logoutput => true,
     }
     
     exec {'install-bundler':
